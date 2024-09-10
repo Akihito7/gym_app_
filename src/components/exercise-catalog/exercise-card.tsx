76675b360a58298ea808a8f98109c729f9a82507
akihito7 <akihitopro7@gmail.com>
2024-09-10 15:21:08 -0300

    checkbox exercise-card

--- src/components/exercise-catalog/exercise-card.tsx
+++ src/components/exercise-catalog/exercise-card.tsx
@@ -2,9 +2,17 @@ import { StyleSheet, View, Text } from "react-native";
 import { defaultTheme } from "../../configs/default-theme";
 import { Avatar } from "react-native-elements";
 import ExerciseImg from "../../../assets/biceps.png"
+import { CheckBox } from "react-native-elements";
+import { useState } from "react";
 
+type ParamsExerciseCard = {
+  id: number;
+  name: string;
+  group: string;
+}
 
-export function ExerciseCard() {
+export function ExerciseCard({ id, name, group }: ParamsExerciseCard) {
+  const [checked, setChecked] = useState(false);
   return (
     <View style={styles.container}>
       <Avatar
@@ -13,9 +21,14 @@ export function ExerciseCard() {
         avatarStyle={{ borderRadius: 10 }}
       />
       <View style={styles.containerExerciseInfo}>
-        <Text style={styles.primaryText}>Biceps barra w</Text>
-        <Text style={styles.secondaryText}>Biceps</Text>
+        <Text style={styles.primaryText} numberOfLines={1}>{name}</Text>
+        <Text style={styles.secondaryText}>{group}</Text>
       </View>
+
+      <CheckBox size={28} checked={checked} onPress={() => {
+        console.log("Exercise selected =>", id)
+        setChecked(prev => !prev)
+      }} />
     </View>
   )
 }
@@ -23,16 +36,17 @@ export function ExerciseCard() {
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    alignItems : "center",
+    alignItems: "center",
     flexDirection: "row",
     backgroundColor: defaultTheme.colors.backgroundComponents,
     borderRadius: 10,
-    paddingRight : 12,
+    paddingRight: 12,
   },
   containerExerciseInfo: {
     flex: 1,
     justifyContent: "center",
     marginLeft: 16,
+    paddingRight : 20,
   },
   primaryText: {
     fontSize: 16,
--- src/screens/exercise-catalog-screen.tsx
+++ src/screens/exercise-catalog-screen.tsx
@@ -1,19 +1,58 @@
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Header } from "../components/header";
 import { Input } from "../components/input";
 import { ExerciseCard } from "../components/exercise-catalog/exercise-card";
+import { defaultTheme } from "../configs/default-theme";
 
+const exercises = [
+  {
+    "id": 1,
+    "name": "Supino Reto",
+    "group": "Peito"
+  },
+  {
+    "id": 2,
+    "name": "Agachamento",
+    "group": "Pernas"
+  },
+  {
+    "id": 3,
+    "name": "Puxada na Barra Fixa",
+    "group": "Costas"
+  },
+  {
+    "id": 4,
+    "name": "Rosca Direta",
+    "group": "Bíceps"
+  }
+];
 
 export function ExerciseCatalogScreen() {
   return (
     <View style={styles.container}>
       <Header title="Escolher exercicios" />
-
       <View style={styles.main}>
         <Input />
-
-        <ExerciseCard />
-
+        <FlatList
+          data={exercises}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <ExerciseCard
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              group={item.group}
+            />)}
+          ItemSeparatorComponent={() => <View style={{ marginTop: 8 }} />}
+          showsVerticalScrollIndicator={false}
+          style={{
+            marginBottom: 18,
+            marginTop: 24,
+          }}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
@@ -29,5 +68,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     marginBottom: 64,
     paddingTop: 24,
+  },
+  button: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    backgroundColor: defaultTheme.colors.button,
+    height: 52,
+    marginBottom: 12,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "semibold",
+    color: defaultTheme.colors.primaryText,
   }
 })
