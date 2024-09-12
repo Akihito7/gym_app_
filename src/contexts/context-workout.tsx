9679c6e31e62e544d726be2fee25303de121b379
akihito7 <akihitopro7@gmail.com>
2024-09-12 14:48:54 -0300

    finishe context

--- src/contexts/context-workout.tsx
+++ src/contexts/context-workout.tsx
@@ -1,14 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-type TypeContextWorkout = {}
+type TypeContextWorkout = {
+    workoutSession: TypeWorkoutSession,
+    setWorkoutSession: React.Dispatch<React.SetStateAction<TypeWorkoutSession>>
+}
 export const ContextWorkout = createContext({} as TypeContextWorkout);
 
 type TypeContextWorkoutProvider = {
-    children : React.ReactNode
+    children: React.ReactNode
 }
-export function ContextWorkoutProvider({ children } : TypeContextWorkoutProvider){
+
+export type TypeWorkoutSession = {
+    id: number | null;
+    name: string | null;
+    exercises: {
+        id: string;
+        name: string;
+        group: string;
+        gif: string;
+        description: string;
+    }[]
+}
+
+export function ContextWorkoutProvider({ children }: TypeContextWorkoutProvider) {
+    const [workoutSession, setWorkoutSession] = useState<TypeWorkoutSession>({} as TypeWorkoutSession)
     return (
-        <ContextWorkout.Provider value={{}}>
+        <ContextWorkout.Provider value={{
+            workoutSession,
+            setWorkoutSession
+        }}>
             {children}
         </ContextWorkout.Provider>
     )
--- src/hooks/useContextWorkout.tsx
+++ src/hooks/useContextWorkout.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { ContextWorkout } from "../contexts/context-workout";
 
+
 export function useContextWorkout() {
     return useContext(ContextWorkout)
 }
